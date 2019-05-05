1f0b7b580da55ed3c70aed604f63270c6b58a4f6
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2019-05-05 10:40:30 +0300

    Fixes

---
+++ Tinylog/Application/Enums.swift
@@ -0,0 +1,18 @@
+//
+//  Config.swift
+//  Tinylog
+//
+//  Created by Spiros Gerokostas on 27/04/2019.
+//  Copyright © 2019 Spiros Gerokostas. All rights reserved.
+//
+
+enum Notifications {
+    static let fontDidChangeNotification = "TLIFontDidChangeNotification"
+}
+
+enum EnvUserDefaults {
+    static let syncMode = "TLISyncMode"
+    static let fontSize = "kFontSize"
+    static let systemFontSize = "kSystemFontSize"
+    static let setupScreen = "kSetupScreen"
+}
