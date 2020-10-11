f9d8c34a5930ebd549ce3c0dc2231ec945614b1a
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2020-10-11 12:09:48 +0300

    feat: add view model for menu colors view

---
+++ Tinylog/ViewModels/MenuColorsViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  MenuColorsViewModel.swift
+//  Tinylog
+//
+//  Created by Spiros Gerokostas on 11/10/20.
+//  Copyright © 2020 Spiros Gerokostas. All rights reserved.
+//
+
+import Foundation
+
+struct MenuColorsViewModel {
+
+    private(set) var index: Int = 0
+    let colors: [String] = ["#6a6de2", "#008efe", "#fe4565", "#ffa600", "#50de72", "#ffd401"]
+
+    mutating func configure(list: TLIList?) {
+        guard let list = list else {
+            return
+        }
+        if let color = list.color {
+            index = findIndexByColor(color)
+        }
+    }
+
+    private func findIndexByColor(_ color: String) -> Int {
+        colors.indexes(of: color).first ?? 0
+    }
+}
+
+extension Array where Element: Equatable {
+    func indexes(of element: Element) -> [Int] {
+        enumerated()
+        .filter { element == $0.element }
+        .map { $0.offset }
+    }
+}
