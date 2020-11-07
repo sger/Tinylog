c44e080db39e94e13e0915d7fb8ddd507b892ef5
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2020-11-07 18:43:46 +0200

    feat: add unit tests for tasks view model

--- Tinylog/ViewModels/Tasks/TasksViewModel.swift
+++ Tinylog/ViewModels/Tasks/TasksViewModel.swift
@@ -9,23 +9,23 @@
 import Foundation
 
 final class TasksViewModel {
-    
+
     private let managedObjectContext: NSManagedObjectContext
-    
+
     init(managedObjectContext: NSManagedObjectContext) {
         self.managedObjectContext = managedObjectContext
     }
-    
+
     func exportUnarchivedTasks(with list: TLIList) -> String {
         let tasks = TLITask.fetchUnarchivedTasks(with: managedObjectContext, list)
         var output = ""
-        
+
         output = output.appending("\(list.title ?? "")\n")
-        
+
         tasks.forEach {
             output = output.appending("- \($0.displayLongText ?? "")\n")
         }
-        
-        return output
+
+        return output.trimmingCharacters(in: .whitespacesAndNewlines)
     }
 }
