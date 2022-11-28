cc4ec3fac8becc3b7f4f58cdad43437dc19a4a28
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2022-11-28 10:03:02 +0200

    Fix linting warnings

--- Tinylog/Helpers/Utils.swift
+++ Tinylog/Helpers/Utils.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 open class Utils {
-    open class func delay(_ delay: Double, closure:@escaping () -> Void) {
+    open class func delay(_ delay: Double, closure: @escaping () -> Void) {
         DispatchQueue.main.asyncAfter(
             deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC),
             execute: closure)
